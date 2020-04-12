432727e4da7dada94585598885927bea65bb2665
Stefan Nožinić <stefan@lugons.org>
2020-04-12 13:15:51 +0200

    Modify build script to not download from maven repository every time

--- backend/build.sh
+++ backend/build.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
+set -x 
 
 BACKEND_DIR="$(readlink -f $(dirname $0))"
+echo $BACKEND_DIR
 pushd .
 cd "$BACKEND_DIR"
 APPNAME="$(ls -d *-ear | sed "s/-ear//g")"
 popd
 
+DOCKER_OPTIONS=""
+for t in ejb ear web; do 
+	DOCKER_OPTIONS="${DOCKER_OPTIONS} -v $BACKEND_DIR/$APPNAME-$t:/usr/src/mymaven/$APPNAME-$t"
+done
 docker run \
 	-it --rm \
 	--name prodex_api_build \
-	-v "$BACKEND_DIR/":/usr/src/mymaven/ \
+	-v "$BACKEND_DIR/pom.xml":/usr/src/mymaven/pom.xml \
+	${DOCKER_OPTIONS} \
+	-v "$BACKEND_DIR/.m2":/root/.m2 \
 	-w /usr/src/mymaven \
 	maven mvn clean package
 
