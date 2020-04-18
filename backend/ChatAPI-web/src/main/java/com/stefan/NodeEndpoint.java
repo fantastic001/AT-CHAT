78eb3c0be19a5ba6071724783f9cc7670b441412
Stefan Nožinić <stefan@lugons.org>
2020-04-18 22:43:12 +0200

    Endpoint skeleton

---
+++ backend/ChatAPI-web/src/main/java/com/stefan/NodeEndpoint.java
@@ -0,0 +1,31 @@
+package com.stefan;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import javax.servlet.http.*;
+import javax.ws.rs.core.*;
+
+import java.util.Date;
+
+@Path("node")
+public class NodeEndpoint {
+	//@Context private HttpServletRequest request;
+
+
+	@GET	
+	@Path("say")
+	@Produces("application/json")
+	public Result hello() {
+		Result r = new Result();
+		r.setA(5);
+		return r;
+	}
+}
---
+++ backend/ChatAPI-web/src/main/java/com/stefan/NodesEndpoint.java
@@ -0,0 +1,32 @@
+package com.stefan;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import javax.servlet.http.*;
+import javax.ws.rs.core.*;
+
+import java.util.Date;
+
+@Path("nodes")
+public class NodesEndpoint {
+	//@Context private HttpServletRequest request;
+
+
+
+	@GET	
+	@Path("say")
+	@Produces("application/json")
+	public Result hello() {
+		Result r = new Result();
+		r.setA(5);
+		return r;
+	}
+}
